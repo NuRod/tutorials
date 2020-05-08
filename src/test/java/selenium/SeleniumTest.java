655fc530e15d5b28f34253496f2d94380b961ada
nurod <naaralves@gmail.com>
2020-05-08 02:18:19 +0100

    Initial Commit

---
+++ src/test/java/selenium/SeleniumTest.java
@@ -0,0 +1,16 @@
+package selenium;
+import com.codeborne.selenide.Condition;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class SeleniumTest {
+    @Test
+    public void automationTest() {
+        open("http://explorecalifornia.org/contact.htm");
+        $( By.cssSelector("#name")).should(Condition.appear);
+        $( By.cssSelector("#comments")).should(Condition.appear);
+
+    }
+}
