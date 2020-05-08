95dd43cd45c67e6aca064d69c596d08a8d19753a
NuRod <naaralves@gmail.com>
2020-05-08 04:15:07 +0100

    finalizing code

--- src/test/java/selenium/SeleniumTest.java
+++ src/test/java/selenium/SeleniumTest.java
@@ -2,15 +2,38 @@ package selenium;
 import com.codeborne.selenide.Condition;
 import org.junit.Test;
 import org.openqa.selenium.By;
-//
-import static com.codeborne.selenide.Selenide.*;
 
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.*;
+//import static com.codeborne.selenide.Condition.*;
+/* Selenide
+1. Open the page
+2. $(element).doAction()
+3. $(element).check(condition)
+
+  open("/login");
+  $("#submit").click();
+  $(".message").shouldHave(text("Hello"));
+*/
 public class SeleniumTest {
     @Test
     public void automationTest() {
         open("http://explorecalifornia.org/contact.htm");
-        $( By.cssSelector("#name")).should(Condition.appear);
-        $( By.cssSelector("#comments")).should(Condition.appear);
+        $( "#name").should(appear);
+        $( "#comments").should(appear);
 
+        $( "#name").setValue("Nuno");
+
+        $(By.name("state")).should(appear).selectOptionContainingText("Texas");
+        $( "#backpack").click();
+        $(By.name("newsletter")).selectRadio("yes");
+
+        $( "#name").shouldHave(Condition.value("Nuno"));
+        String state = $("#state").should(appear).getSelectedText();
+        assert(state).equals("Texas");
+
+        $( "#backpack").shouldBe(selected);
+        $(By.name("newsletter")).shouldHave(value("yes"));
     }
 }
