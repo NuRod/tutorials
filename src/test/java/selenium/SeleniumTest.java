beffc1af90b447f552c134be2a2962c804b024ad
nurod <naaralves@gmail.com>
2020-05-08 02:28:45 +0100

    adding comment

--- src/test/java/selenium/SeleniumTest.java
+++ src/test/java/selenium/SeleniumTest.java
@@ -2,7 +2,7 @@ package selenium;
 import com.codeborne.selenide.Condition;
 import org.junit.Test;
 import org.openqa.selenium.By;
-
+//
 import static com.codeborne.selenide.Selenide.*;
 
 public class SeleniumTest {
